4866a93854bf8a68cadd3fd5d859a2cda6d7644d
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-08-01 23:31:32 +0100

    view model added

---
+++ Final project PresentationLayer/Models/DonationViewModel/CreateDonationViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Final_project_PresentationLayer.Models.DonationViewModel
+{
+    public class CreateDonationViewModel
+    {
+        public string FoodDetails { get; set; } = default!;
+        public int Quantity { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public DateTime PickUpTime { get; set; }
+        public string PickUpLocation { get; set; } = default!;
+        public Guid UserId { get; set; }
+        public IList<IFormFile> DonationImages { get; set; } = null!;
+        public IFormFile PrimaryImageUrl { get; set; } = null!;
+    }
+}
---
+++ Final project PresentationLayer/Models/DonationViewModel/DonationSummaryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Final_project_PresentationLayer.Models.DonationViewModel
+{
+    public class DonationSummaryViewModel
+    {
+        public int PendingDonations { get; set; }
+        public int ApprovedDonations { get; set; }
+        public int DisapprovedDonations { get; set; }
+        public int ReceivedDonations { get; set; }
+    }
+
+}
