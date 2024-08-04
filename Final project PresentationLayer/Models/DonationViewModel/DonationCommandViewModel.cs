f998191d7c1f384e2aaf8589d9eef9ab7a405e6f
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-08-04 23:25:12 +0100

    Search model Added

---
+++ Final project PresentationLayer/Models/DonationViewModel/DonationCommandViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Final_project_PresentationLayer.Models.DonationViewModel
+{
+    public class DonationCommandViewModel
+    {
+        public int DonationType { get; set; }
+        public PaginationFilter filter { get; set; }
+    }
+
+    public class PaginationFilter
+    {
+        public int PageNumber { get; }
+        public int PageSize { get; }
+        public int TotalRecords { get; }
+
+        public PaginationFilter()
+        {
+            PageNumber = 1;
+            PageSize = 10;
+        }
+
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            PageSize = pageSize > 10 ? 10 : pageSize;
+        }
+    }
+}
---
+++ Final project PresentationLayer/Models/DonationViewModel/DonationSearchCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_project_PresentationLayer.Models.DonationViewModel
+{
+    public class DonationSearchCommand
+    {
+        public string? Location { get; set; }
+        public int? MinQuantity { get; set; }
+        public int? MaxQuantity { get; set; }
+        public PaginationFilter Filter { get; set; } = new PaginationFilter();
+    }
+}
